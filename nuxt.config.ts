834e104b27ac224df3d9ba79ff8f42acb7fbc795
Eyumul <eyuelmulugeta60@gmail.com>
2025-02-06 01:09:56 +0300

    add dependency (iconify)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss', 'shadcn-nuxt'
+    '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'
   ],
   shadcn: {
     /**
