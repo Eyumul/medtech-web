2c31dc16d1b484083f738135c62e803a8f87529c
Eyumul <eyuelmulugeta60@gmail.com>
2025-02-05 14:57:26 +0300

    create new nuxt project, add dependencies (tailwind, color-mode, schadcn), add company logo, configure website color

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>
+            <img src="~/assets/images/medtechlogo.png" alt="Medtech Logo" class="w-48 h-auto"/>
+        </div>
+        <slot/>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Dialog>
+      <DialogTrigger>
+        Open Modal
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            this is the modal
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          Save changes
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </div>
+</template>
+
+<script setup>
+
+</script>
